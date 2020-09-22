ef4a13ef16ed7bf2e3861d58bce4f4e1b1f73f53
Dennis Walangadi <dennisaw2000@gmail.com>
2020-09-22 09:24:47 +0700

    read zips

--- scele_grader.py
+++ scele_grader.py
@@ -8,15 +8,14 @@ import sys
 # TODO: Modularity
 
 # config
-# TODO(Dennis): runtime config? or file based config?
 config = {
     "asdos_code": "DNS",
-    "asdosee_data": "DNS.csv",                              # csv path relative to file
+    "asdosee_data": "DNS.csv",       # csv path relative to file
+    "lab": "0",
     "tasks": ["nama", "lovemeter"]
 }
 
 cwd = Path.cwd() / sys.argv[0]
-asdosees = []
 
 class Asdosee:
     def __init__(self, data):
@@ -29,23 +28,33 @@ class Asdosee:
 # TASK CLASS
 
 def fetch_asdosee(asdosee_data):
-    with open(config["asdosee_data"], 'r') as file_:
+    asdosees = []
+    with open(config[asdosee_data], 'r') as file_:
         data = reader(file_)
         for row in data:
             asdosee = Asdosee(row)
             asdosees.append(asdosee)
+    return asdosees
         
-def fetch_subs():
+def fetch_subs(lab):
     extensions = ('**/*.zip', '**/*.rar')
-    if not (submission_path := cwd / "submission").exists():
+    if not (submission_path := cwd / lab / "submission").exists():
         raise Exception("submission folder doesn't exist")
     else:
-        
         subs = []
         for extension in extensions:
-            subs.extend(list(submission_path.glob(extension)))
+            subs.extend([files for files in submission_path.glob(extension)])
         filtered = filter(lambda sub: config["asdos_code"] in sub, subs)
         return list(filtered)
     
 if __name__ == "__main__":
     print("UNDER CONSTRUCTION")
+    asdosee = fetch_asdosee(config["asdosee_data"])
+    subs = fetch_subs(config["lab"])
+    
+    # TODO
+    for sub in subs:
+        with ZipFile(sub, "r") as unzip:
+            for task in unzip.namelist():
+                # wait using count (ghetto lmao)
+                getstatusoutput('code -r ' + task).count(0)
